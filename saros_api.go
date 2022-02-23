d2a1cc2bddb8642b66944a3f63d52e56fefd2f2c
pokassun <pokassun@tutanota.com>
2022-02-23 15:47:33 +0800

    use stake/unstake for saros

--- saros_api.go
+++ saros_api.go
@@ -6,44 +6,51 @@ import (
 	"github.com/gagliardetto/solana-go"
 )
 
-type SarosAddLiquidityAccount struct {
-	Owner       solana.PublicKey
+type SarosStakeAccounts struct {
 	Payer       solana.PublicKey
+	Owner       solana.PublicKey
 	LPTokenMint solana.PublicKey
 }
 
-type SarosAddLiquidityData struct {
-	TokenAAmount uint64
-	TokenBAmount uint64
+type SarosStakeData struct {
+	StakeAmountA uint64
+	StakeAmountB uint64
 }
 
-func (c *Client) SarosAddLiquidity(ctx context.Context, accounts SarosAddLiquidityAccount, data SarosAddLiquidityData) (*OperationResponse, error) {
+func (c *Client) SarosStake(ctx context.Context, accounts SarosStakeAccounts, data SarosStakeData) (*OperationResponse, error) {
 	result := OperationResponse{}
-	err := c.post(ctx, "saros/add-liquidity", OperationRequest{
+	err := c.post(ctx, "saros/stake", OperationRequest{
 		Accounts: map[string]interface{}{
-			"ownerAccount": accounts.Owner,
 			"payer":        accounts.Payer,
+			"ownerAccount": accounts.Owner,
 			"lpTokenMint":  accounts.LPTokenMint,
 		},
 		Data: map[string]interface{}{
-			"tokenAAmount": data.TokenAAmount,
-			"tokenBAmount": data.TokenBAmount,
+			"stakeAmountA": data.StakeAmountA,
+			"stakeAmountB": data.StakeAmountB,
 		},
 	}, &result)
 	return &result, err
 }
 
-func (c *Client) SarosAddLiquiditySimulate(ctx context.Context, accounts SarosAddLiquidityAccount, data SarosAddLiquidityData) (*SimulationResponse, error) {
+type SarosUnstakeAccounts struct {
+	Payer        solana.PublicKey
+	Owner        solana.PublicKey
+	LPTokenMint  solana.PublicKey
+	WithdrawMint solana.PublicKey
+}
+
+func (c *Client) SarosUnstake(ctx context.Context, accounts SarosUnstakeAccounts, unstakeAmount uint64) (*SimulationResponse, error) {
 	result := SimulationResponse{}
-	err := c.post(ctx, "saros/add-liquidity/simulate", OperationRequest{
+	err := c.post(ctx, "saros/unstake", OperationRequest{
 		Accounts: map[string]interface{}{
-			"ownerAccount": accounts.Owner,
 			"payer":        accounts.Payer,
+			"ownerAccount": accounts.Owner,
 			"lpTokenMint":  accounts.LPTokenMint,
+			"withdrawMint": accounts.WithdrawMint,
 		},
 		Data: map[string]interface{}{
-			"tokenAAmount": data.TokenAAmount,
-			"tokenBAmount": data.TokenBAmount,
+			"unstakeAmount": unstakeAmount,
 		},
 	}, &result)
 	return &result, err
--- saros_api_test.go
+++ saros_api_test.go
@@ -9,40 +9,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSarosAddLiquidity(t *testing.T) {
+func TestSarosStake(t *testing.T) {
 	infra := GetTestInfra(t)
 	// LP C98-CASH
-	res, err := infra.Client.SarosAddLiquidity(context.Background(),
-		SarosAddLiquidityAccount{
-			Owner:       solana.MustPublicKeyFromBase58("6uHZK4v5fz9JZX6VaVyxus5AxFqJk7B8jiHw1gMYaSZg"),
+	res, err := infra.Client.SarosStake(context.Background(),
+		SarosStakeAccounts{
 			Payer:       solana.MustPublicKeyFromBase58("6uHZK4v5fz9JZX6VaVyxus5AxFqJk7B8jiHw1gMYaSZg"),
-			LPTokenMint: solana.MustPublicKeyFromBase58("4g7DwZVn8edcSYR34zE4Dm13YLH6ZvtXCNnaUyQSFy9w"), // PAI
+			Owner:       solana.MustPublicKeyFromBase58("6uHZK4v5fz9JZX6VaVyxus5AxFqJk7B8jiHw1gMYaSZg"),
+			LPTokenMint: solana.MustPublicKeyFromBase58("4g7DwZVn8edcSYR34zE4Dm13YLH6ZvtXCNnaUyQSFy9w"),
 		},
-		SarosAddLiquidityData{
-			TokenAAmount: 0,
-			TokenBAmount: 100_000, //0.1 CASH
+		SarosStakeData{
+			StakeAmountA: 0,
+			StakeAmountB: 100_000, //0.1 CASH
 		},
 	)
 	assert.NoError(t, err)
 	fmt.Println(res)
 }
 
-func TestSarosAddLiquiditySimulate(t *testing.T) {
+func TestSarosUnstakeSimulate(t *testing.T) {
 	infra := GetTestInfra(t)
 
-	res, err := infra.Client.SarosAddLiquiditySimulate(context.Background(),
-		SarosAddLiquidityAccount{
-			Owner:       solana.MustPublicKeyFromBase58("6uHZK4v5fz9JZX6VaVyxus5AxFqJk7B8jiHw1gMYaSZg"),
-			Payer:       solana.MustPublicKeyFromBase58("6uHZK4v5fz9JZX6VaVyxus5AxFqJk7B8jiHw1gMYaSZg"),
-			LPTokenMint: solana.MustPublicKeyFromBase58("4g7DwZVn8edcSYR34zE4Dm13YLH6ZvtXCNnaUyQSFy9w"), // PAI
+	res, err := infra.Client.SarosUnstake(context.Background(),
+		SarosUnstakeAccounts{
+			Payer:        solana.MustPublicKeyFromBase58("6uHZK4v5fz9JZX6VaVyxus5AxFqJk7B8jiHw1gMYaSZg"),
+			Owner:        solana.MustPublicKeyFromBase58("6uHZK4v5fz9JZX6VaVyxus5AxFqJk7B8jiHw1gMYaSZg"),
+			LPTokenMint:  solana.MustPublicKeyFromBase58("4g7DwZVn8edcSYR34zE4Dm13YLH6ZvtXCNnaUyQSFy9w"),
+			WithdrawMint: solana.MustPublicKeyFromBase58("C98A4nkJXhpVZNAZdHUA95RpTF3T4whtQubL3YobiUX9"), // C98
 		},
-		SarosAddLiquidityData{
-			TokenAAmount: 1_000_000, // 1 PRT
-			TokenBAmount: 0,
-		},
+		100_000, // 1 PRT
 	)
-
 	assert.NoError(t, err)
-	// LP token mint
-	assert.Greater(t, res.Balances["4g7DwZVn8edcSYR34zE4Dm13YLH6ZvtXCNnaUyQSFy9w"], 0)
+	fmt.Println(res)
 }
